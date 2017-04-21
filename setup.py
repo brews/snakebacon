04cb3b30da937ce77a336da533924e4bb4418f34
Brewster Malevich <sbmalev@gmail.com>
2017-04-21 14:42:36 -0700

    Corrected setup.py and baconwrap for correct compiling. Move *.c files to *.cpp.

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 
-module1 = Extension("baconwrap",
-                    sources=["snakebacon/bacon/baconwrap.pyx"],
+bacon = Extension("baconwrap",
+                    sources=["snakebacon/bacon/baconwrap.pyx", "snakebacon/bacon/input.cpp", "snakebacon/bacon/Matrix.cpp", "snakebacon/bacon/ranfun.cpp", "snakebacon/bacon/vector.cpp", "snakebacon/bacon/kernel.cpp"],
                     language="c++",
-                    library_dirs=["/usr/local/lib"],
-                    # include_dirs=["/usr/local/include", "snakebacon/bacon"],
+                    library_dirs=["/usr/local/lib"],  # TODO(brews): Use platform agnostic #includes
+                    include_dirs=["snakebacon/bacon"],  # TODO(brews): Use platform agnostic #includes
                     libraries=["gsl", "gslcblas", "m"],
-                    extra_compile_args=["-O2", "-fopenmp", "-D_FILE_OFFSET_BITS=64", "-Wno-write-strings"])
+                    extra_compile_args=["-xc++", "-lstdc++", "-shared-libgcc", "-O2", "-fopenmp", "-D_FILE_OFFSET_BITS=64", "-Wno-write-strings"])
 
 
 setup(name = 'snakebacon',
-      ext_modules = cythonize([module1]))
+      # ext_modules=cythonize([bacon], , gdb_debug=True)  # To debug, then do something like `cython --gdb baconwrap.pyx`
+      ext_modules=cythonize([bacon]))
--- snakebacon/bacon/baconwrap.pyx
+++ snakebacon/bacon/baconwrap.pyx
@@ -2,11 +2,12 @@ from libc.stdlib cimport malloc, free
 
 
 def cook(str infile, str outfile, int ssize):
-    cdef extern from "bacon.c":
+    cdef extern from "bacon.cpp":
         int main(int argc, char *argv[])
     cdef char **outgoing_argv
     cdef bytes binfile
     cdef bytes boutfile
+    cdef bytes bssize
     cdef char* cinfile
     cdef char* coutfile
     cdef char* cssize
@@ -15,11 +16,19 @@ def cook(str infile, str outfile, int ssize):
     try:
         binfile = infile.encode('utf8')
         cinfile = binfile
+
         boutfile = outfile.encode('utf8')
         coutfile = boutfile
+
+        bssize = str(ssize).encode('utf8')
+        cssize = bssize
+
         outgoing_argv[1] = cinfile
         outgoing_argv[2] = coutfile
         outgoing_argv[3] = cssize
         return main(argcount, outgoing_argv)
     finally:
         free(outgoing_argv)
+
+
+# cook('MSB2K_20.bacon', 'out.bacon', 2000)
