5bdb2ead235ad9d398ef54faeb0071e63b4d6103
Brewster Malevich <sbmalev@gmail.com>
2017-04-28 21:48:55 -0700

    Add prelim docstring for bacon module.

---
+++ snakebacon/bacon/__init__.py
@@ -0,0 +1,4 @@
+"""
+Cython wrapper for the C/C++ from Bacon age-modelling software (http://chrono.qub.ac.uk/blaauw/bacon.html) written by 
+Maarten Blaauw (maarten.blaauw@qub.ac.uk) and  Andres Christen (jac@cimat.mx).
+"""
