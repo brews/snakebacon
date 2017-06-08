95507c47d07e2842659548f2aa28591a9c147694
Brewster Malevich <sbmalev@gmail.com>
2017-06-07 19:11:51 -0700

    setup.py style cleanup.

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 bacon = Extension("snakebacon.bacon.baconwrap",
-                  sources=["snakebacon/bacon/baconwrap.pyx", "snakebacon/bacon/input.cpp",
-                           "snakebacon/bacon/Matrix.cpp", "snakebacon/bacon/ranfun.cpp", "snakebacon/bacon/vector.cpp",
+                  sources=["snakebacon/bacon/baconwrap.pyx",
+                           "snakebacon/bacon/input.cpp",
+                           "snakebacon/bacon/Matrix.cpp",
+                           "snakebacon/bacon/ranfun.cpp",
+                           "snakebacon/bacon/vector.cpp",
                            "snakebacon/bacon/kernel.cpp"],
                   language="c++",
                   libraries=["gsl", "openblas"],
-                  extra_compile_args=["-xc++", "-lstdc++", "-shared-libgcc", "-O2", "-fopenmp"])  # ,
+                  extra_compile_args=["-xc++", "-lstdc++", "-shared-libgcc",
+                                      "-O2", "-fopenmp"])
 
 setup(name='snakebacon',
       ext_modules=cythonize([bacon]))
