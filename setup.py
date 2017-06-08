c175d15904d58e0a551ac2027c300e8add059cc3
Brewster Malevich <sbmalev@gmail.com>
2017-06-07 20:16:47 -0700

    Expanded setup, packaging, and travis files.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from distutils.core import setup, Extension
-
 from Cython.Build import cythonize
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
 
 bacon = Extension("snakebacon.bacon.baconwrap",
                   sources=["snakebacon/bacon/baconwrap.pyx",
@@ -15,4 +15,25 @@ bacon = Extension("snakebacon.bacon.baconwrap",
                                       "-O2", "-fopenmp"])
 
 setup(name='snakebacon',
+      version='0.0.1',
+      description='snakebacon',
+      url='https://github.com/brews/snakebacon',
+      author='S. Brewster Malevich',
+      author_email='malevich@email.arizona.edu',
+      license='GPLv3',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Software Development',
+
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+          'Programming Language :: Python :: 3'],
+      keywords='marine radiocarbon c14',
+      packages=find_packages(exclude=['contrib', 'docs', 'build']),
+      install_requires=['setuptools', 'numpy', 'cython', 'pandas', 'matplotlib'],
+      extras_require={'dev': ['check-manifest']},
       ext_modules=cythonize([bacon]))
