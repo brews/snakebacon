07b8e1da6c0dab0e2d351883bf691d1bda150409
Brewster Malevich <sbmalev@gmail.com>
2017-06-27 14:11:19 +0800

    Changed docs conf to use numpydoc and rtd themes.

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.extlinks',
               'sphinx.ext.mathjax',
               'sphinx.ext.autodoc',
-              # 'numpydoc',
+              'numpydoc',
               ]
 
 extlinks = {'issue': ('https://github.com/brews/snakebacon/issues/%s', 'GH'),
@@ -100,14 +100,14 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-# if not on_rtd:  # only import and set the theme if we're building docs locally
-#     import sphinx_rtd_theme
-    # html_theme = 'sphinx_rtd_theme'
-    # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
