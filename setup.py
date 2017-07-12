f0103ae5611a8c10773c68d01d0361f31ac1679e
Brewster Malevich <sbmalev@gmail.com>
2017-07-12 16:05:04 -0700

    Another hack to workaround bad conda linking in readthedocs builds.

--- setup.py
+++ setup.py
@@ -26,50 +26,50 @@ short_version = '%s'
     finally:
         a.close()
 
-
 write_version_py()
 
-bacon_extension_kwargs = dict(sources = ["snakebacon/bacon/baconwrap.pyx",
-                                         "snakebacon/bacon/input.cpp",
-                                         "snakebacon/bacon/Matrix.cpp",
-                                         "snakebacon/bacon/ranfun.cpp",
-                                         "snakebacon/bacon/vector.cpp",
-                                         "snakebacon/bacon/kernel.cpp"],
-                              language = "c++",
-                              libraries = ["gsl", "openblas"],
-                              extra_compile_args = ["-xc++", "-lstdc++", "-shared-libgcc",
-                                                    "-O2", "-fopenmp"])
+bacon = Extension("snakebacon.bacon.baconwrap",
+                  sources=["snakebacon/bacon/baconwrap.pyx",
+                           "snakebacon/bacon/input.cpp",
+                           "snakebacon/bacon/Matrix.cpp",
+                           "snakebacon/bacon/ranfun.cpp",
+                           "snakebacon/bacon/vector.cpp",
+                           "snakebacon/bacon/kernel.cpp"],
+                  language="c++",
+                  libraries=["gsl", "openblas"],
+                  extra_compile_args=["-xc++", "-lstdc++", "-shared-libgcc", "-O2", "-fopenmp"],
+                  )
+
+setup_kwargs = dict(name='snakebacon',
+                    version='0.0.1',
+                    description='snakebacon',
+                    url='https://github.com/brews/snakebacon',
+                    author='S. Brewster Malevich',
+                    author_email='malevich@email.arizona.edu',
+                    license='GPLv3',
+                    classifiers=[
+                        'Development Status :: 3 - Alpha',
+
+                        'Operating System :: POSIX',
+
+                        'Intended Audience :: Developers',
+                        'Intended Audience :: Science/Research',
+
+                        'Topic :: Scientific/Engineering',
+                        'Topic :: Software Development',
+
+                        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+                        'Programming Language :: Python :: 3'],
+                    keywords='marine radiocarbon c14',
+                    install_requires=['setuptools', 'numpy', 'Cython', 'pandas', 'matplotlib', 'scipy'],
+                    extras_require={'dev': ['check-manifest']},
+                    packages=find_packages(),
+                    package_data={'snakebacon': ['tests/*.csv', 'bacon/Curves/*.14C']},
+                    )
 
 # Deal with bad linking bug with conda in readthedocs builds.
-if os.environ.get('READTHEDOCS') == 'True':
-    bacon_extension_kwargs['include_dirs'] = ['~/miniconda3/envs/snakebacon-docs/include']
+if os.environ.get('READTHEDOCS') != 'True':
+    setup_kwargs['ext_modules'] = cythonize([bacon])
 
-bacon = Extension("snakebacon.bacon.baconwrap", **bacon_extension_kwargs)
-
-setup(name='snakebacon',
-      version='0.0.1',
-      description='snakebacon',
-      url='https://github.com/brews/snakebacon',
-      author='S. Brewster Malevich',
-      author_email='malevich@email.arizona.edu',
-      license='GPLv3',
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-
-          'Operating System :: POSIX',
-
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-
-          'Topic :: Scientific/Engineering',
-          'Topic :: Software Development',
-
-          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-
-          'Programming Language :: Python :: 3'],
-      keywords='marine radiocarbon c14',
-      install_requires=['setuptools', 'numpy', 'Cython', 'pandas', 'matplotlib', 'scipy'],
-      extras_require={'dev': ['check-manifest']},
-      packages=find_packages(),
-      package_data={'snakebacon': ['tests/*.csv', 'bacon/Curves/*.14C']},
-      ext_modules=cythonize([bacon]))
+setup(**setup_kwargs)
